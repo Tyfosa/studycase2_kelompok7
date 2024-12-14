e9f4c526a89280b1275907a41c1069dbfab4058d
MariaSavira <mariasavira2006@gmail.com>
2024-12-14 23:27:54 +0700

    commit file full

--- Kelompok7.java
+++ Kelompok7.java
@@ -1,56 +1,5 @@
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class Kelompok7 {
-    static Scanner sc = new Scanner(System.in);
-    static String menuKafe[][] = {{"Kopi Hitam", "15000"}, {"Latte", "22000"}, {"Teh Tarik", "12000"}, {"Mie Goreng", "18000"}};
-    static String namaPelanggan;
-    static int nomorMeja, totalHarga;
-
-    //TAMBAH PESANAN & VALIDASI
-    static void tambahPesanan(){
-        
-    }
-    
-    //TAMPILKAN PESANAN
-    static void tampilPesanan(){
-
-    }
-
-    //BAYAR PESANAN
-    static void bayarPesanan(){
-
-    }
-
-    //CETAK STRUK
-    static void cetakStruk(){
-
-    }
-
-    public static void main(String[] args) {
-        int menu;
-
-        System.out.println("===== MENU UTAMA =====");
-        System.out.println("1. Tambahkan Pesanan");
-        System.out.println("2. Tampilkan Daftar Pesanan");
-        System.out.println("3. Pembayaran dan Cetak Struk");
-        System.out.println("4. Keluar");
-        System.out.print("Pilih menu : ");
-        menu = sc.nextInt();
-
-        switch (menu){
-            case 1:
-            tambahPesanan();
-            break;
-            case 2:
-            tampilPesanan();
-            break;
-            case 3:
-            bayarPesanan();
-            case 4:
-            System.out.println("Terima kasih telah berkunjung ke kafe kami!");
-            break;
-=======
 public class Kelompok7{
     static Scanner sc = new Scanner(System.in);
     static String menuKafe[][] = {{"Kopi Hitam", "15000"}, {"Latte", "22000"}, {"Teh Tarik", "12000"}, {"Mie Goreng", "18000"}};
@@ -151,7 +100,7 @@ public class Kelompok7{
     //PEMBAYARAN PESANAN
     static void pembayaranPesanan() {
         System.out.print("\nMasukkan Nama Pelanggan: ");
-        sc.nextLine(); // Membersihkan buffer
+        sc.nextLine(); 
         String nama = sc.nextLine();
     
         boolean ditemukan = false;
@@ -215,7 +164,6 @@ public class Kelompok7{
                     System.out.println("\nTerima kasih telah berbelanja di kafe kami!");
                     return;
             }
->>>>>>> 3d8fbeb4056f6ddfaa2af6133da60f08ed1d0807
         }
     }
 }
